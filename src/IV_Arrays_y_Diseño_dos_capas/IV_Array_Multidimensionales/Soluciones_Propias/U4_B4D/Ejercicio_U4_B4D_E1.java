157b82b2253f9b462c3b68b996a9b7bcb395b381
it-rafamartinez <it-rafamartinez@outlook.es>
2023-01-31 18:22:37 +0100

    B4D

--- src/IV_Arrays_y_Diseño_dos_capas/IV_Array_Multidimensionales/Soluciones_Propias/U4_B4D/Ejercicio_U4_B4D_E1.java
+++ src/IV_Arrays_y_Diseño_dos_capas/IV_Array_Multidimensionales/Soluciones_Propias/U4_B4D/Ejercicio_U4_B4D_E1.java
@@ -11,9 +11,68 @@ package IV_Arrays_y_Diseño_dos_capas.IV_Array_Multidimensionales.Soluciones_Pro
 	matemáticas (x,y) para indicar la ubicación de un árbol. La clase es 
 	capaz de imprimir un pequeño plano tal cual lo dibuja el usuario en su 
 	plano de coordenadas x,y. 
+        Debe funcionar el siguiente main y producir la salida indicada
+class Unidad4{
+        public static void main(String[] args) {
+                //se indica al constructor columnasxfilas
+                PlanoJardin plano=new PlanoJardin(5,4);
+                System.out.println("antes de plantar");
+                plano.imprimirPlano();
+                plano.plantarArbol(0, 3);
+                plano.plantarArbol(4,3);
+                plano.plantarArbol(2, 0);
+                System.out.println("despues de plantar");
+                plano.imprimirPlano();
+                System.out.println(plano.hayArbol(0,3));
+                System.out.println(plano.hayArbol(3,0));
+        }
+}
 */
 
 public class Ejercicio_U4_B4D_E1 {
 
-	public static void main(String[] args) {}
+  public static void main(String[] args) {
+    //se indica al constructor columnasxfilas
+    PlanoJardin plano = new PlanoJardin(5, 4);
+    System.out.println("antes de plantar");
+    plano.imprimirPlano();
+    plano.plantarArbol(0, 3);
+    plano.plantarArbol(4, 3);
+    plano.plantarArbol(2, 0);
+    System.out.println("despues de plantar");
+    plano.imprimirPlano();
+  }
+}
+
+class PlanoJardin {
+
+  // PROPERTIES
+  private char[][] jardin;
+  private int ancho;
+  private int alto;
+
+  // CONSTRUCTORS
+  public PlanoJardin(int alto, int ancho) {
+    this.alto = alto;
+    this.ancho = ancho;
+    this.jardin = new char[ancho][alto];
+    for (int fila = 0; fila < this.jardin.length; fila++) {
+      for (int col = 0; col < this.jardin[fila].length; col++) {
+        jardin[fila][col] = '-';
+      }
+    }
+  }
+
+  public void imprimirPlano() {
+    for (int fila = 0; fila < this.jardin.length; fila++) {
+      for (int col = 0; col < this.jardin[fila].length; col++) {
+        System.out.print(jardin[fila][col] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public void plantarArbol(int posY, int posX) {
+    jardin[posX][posY] = 'A';
+  }
 }
